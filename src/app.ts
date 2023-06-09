f6a14b47ecfe01897785a205731b6ac4a2e045fa
Dennis Ngeno <dennis.ngeno.andela@alation.com>
2023-06-09 12:31:45 +0300

    - added prettier and eslint

--- src/app.ts
+++ src/app.ts
@@ -1,30 +1,30 @@
-import express, {Express, Request, Response} from "express";
-import bodyParser from "body-parser";
-import cors from "cors";
-import UserAuthRoutes from "./routes/userAuthRoutes";
-import CustomerCareAgentRoutes from "./routes/customerCareAgentAuthRoutes";
-import RiderAuthRoutes from "./routes/RiderAuthRoutes";
-import mpesaRoutes from "./routes/mpesaRoutes";
+import express, { Express, Request, Response } from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import UserAuthRoutes from './routes/userAuthRoutes';
+import CustomerCareAgentRoutes from './routes/customerCareAgentAuthRoutes';
+import RiderAuthRoutes from './routes/RiderAuthRoutes';
+import mpesaRoutes from './routes/mpesaRoutes';
 
-import UserOTPRoutes from './routes/userOTPRoutes'
+import UserOTPRoutes from './routes/userOTPRoutes';
 
 const app: Express = express();
 
 app.use(bodyParser.json());
 app.use(
-    bodyParser.urlencoded({
-        extended: true,
-    })
+	bodyParser.urlencoded({
+		extended: true,
+	})
 );
 app.use(cors());
 //routes
-app.use("/api/users/auth", UserAuthRoutes);
-app.use("/api/riders/auth", RiderAuthRoutes);
-app.use("/api/points", mpesaRoutes);
-app.use("/api/users/auth/OTP", UserOTPRoutes);
-app.use("/api/users/customer_agent", CustomerCareAgentRoutes);
-app.get("/api/", (req: Request, res: Response) => {
-    res.send("test route...");
+app.use('/api/users/auth', UserAuthRoutes);
+app.use('/api/riders/auth', RiderAuthRoutes);
+app.use('/api/points', mpesaRoutes);
+app.use('/api/users/auth/OTP', UserOTPRoutes);
+app.use('/api/users/customer_agent', CustomerCareAgentRoutes);
+app.get('/api/', (req: Request, res: Response) => {
+	res.send('test route...');
 });
 
 export default app;
--- src/helpers/CreateToken.ts
+++ src/helpers/CreateToken.ts
@@ -1,18 +1,17 @@
-import {NextFunction, Request, Response} from 'express';
-import jwt, {JwtPayload} from "jsonwebtoken";
-import * as dotenv from "dotenv";
-import {UserType} from "./enums";
+import { NextFunction, Request, Response } from 'express';
+import jwt, { JwtPayload } from 'jsonwebtoken';
+import * as dotenv from 'dotenv';
+import { UserType } from './enums';
 
 dotenv.config();
 
-const secretKey: string = process.env.TOKEN_SECRET_KEY! || "supersecretkey";
-
+const secretKey: string = process.env.TOKEN_SECRET_KEY! || 'supersecretkey';
 
 interface TokenPayload {
-    first_name: string,
-    last_name: string,
-    id: string,
-    type: UserType
+	first_name: string;
+	last_name: string;
+	id: string;
+	type: UserType;
 }
 
 // {
@@ -24,51 +23,56 @@ interface TokenPayload {
 //     "exp": 1687598419
 // }
 
-export const CreateToken = async (tokenObject: TokenPayload): Promise<string> => {
-    try {
-        const userToken: string = jwt.sign(tokenObject, secretKey, {
-            expiresIn: "30d",
-        });
+export const CreateToken = async (
+	tokenObject: TokenPayload
+): Promise<string> => {
+	try {
+		const userToken: string = jwt.sign(tokenObject, secretKey, {
+			expiresIn: '30d',
+		});
 
-        return userToken;
-    } catch (err) {
-        //console.log(err.message);
-        throw err;
-    }
+		return userToken;
+	} catch (err) {
+		//console.log(err.message);
+		throw err;
+	}
 };
 
 export const VerifyToken = async (token: string): Promise<boolean> => {
-    try {
-        const isValid = await jwt.verify(token, secretKey);
-        console.log(isValid);
-        return !!isValid;
-    } catch (err) {
-        return false;
-    }
+	try {
+		const isValid = await jwt.verify(token, secretKey);
+		console.log(isValid);
+		return !!isValid;
+	} catch (err) {
+		return false;
+	}
 };
 
-
 export interface CustomRequest extends Request {
-    payload: JwtPayload;
+	payload: JwtPayload;
 }
 
 export const auth = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const token = req.headers.authorization ? req.headers?.authorization!.split(" ")[1] : "";
-        if (!token) {
-            return res.status(401).json({message: 'Please pass authentication token'});
-        }
+	try {
+		const token = req.headers.authorization
+			? req.headers?.authorization!.split(' ')[1]
+			: '';
+		if (!token) {
+			return res
+				.status(401)
+				.json({ message: 'Please pass authentication token' });
+		}
 
-        jwt.verify(token, secretKey, function (error, decoded: any) {
-            if (error) {
-                return res.status(401).json(error)
-            }
-            if (decoded) {
-                (req as CustomRequest).payload = decoded;
-            }
-        });
-        next();
-    } catch (err) {
-        return res.status(401).json({message: 'Invalid token'});
-    }
+		jwt.verify(token, secretKey, function (error, decoded: any) {
+			if (error) {
+				return res.status(401).json(error);
+			}
+			if (decoded) {
+				(req as CustomRequest).payload = decoded;
+			}
+		});
+		next();
+	} catch (err) {
+		return res.status(401).json({ message: 'Invalid token' });
+	}
 };
