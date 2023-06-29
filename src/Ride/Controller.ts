6998651187640a9143a925826ed1758087edafde
Dennis Ngeno <dennis.ngeno.andela@alation.com>
2023-06-29 12:16:31 +0300

    - initial commit

--- src/Ride/Controller.ts
+++ src/Ride/Controller.ts
@@ -1,41 +1,36 @@
 import { Request, Response } from 'express';
 import s2 from '@radarlabs/s2';
+import { AsyncLocalStorage } from 'node:async_hooks';
+
+const GetUserLatLong = (userId: string, LongLat: [number, number]) => {
+  return [
+    userId,
+    new s2.CellId(new s2.LatLng(LongLat[1], LongLat[0])).parent(9),
+  ];
+};
+
+const riders: Array<Array<any>> = [
+  ['r1', [-1.201726, 36.785418]],
+  ['r2', [-1.207343, 36.786401]],
+  ['r3', [-1.204948, 36.776498]],
+  ['r4', [-1.094669, 36.631879]],
+  ['r5', [-1.206408, 36.791688]],
+];
 
 export const GeoHarsh = (req: Request, res: Response) => {
-  const user1LongLat = [-73.95772933959961, 40.71623280185081];
-  const user2LongLat = [-73.95927429199219, 40.71629785715124];
-  const user3LongLat = [-73.99206161499023, 40.688708709249646];
-
-  interface Group {
-    userId: string;
-    cellId: any;
-  }
-
-  const user1S2: [string, any] = [
-    'user1',
-    new s2.CellId(new s2.LatLng(user1LongLat[1], user1LongLat[0])).parent(13),
-  ];
-  const user2S2: [string, any] = [
-    'user2',
-    new s2.CellId(new s2.LatLng(user2LongLat[1], user2LongLat[0])).parent(13),
-  ];
-  const user3S2: [string, any] = [
-    'user3',
-    new s2.CellId(new s2.LatLng(user3LongLat[1], user3LongLat[0])).parent(13),
-  ];
-
-  let groups: any; // Initialize groups as an empty object
-
-  [user1S2, user2S2, user3S2].forEach(([userId, cellId]) => {
+  let ridersArray = [];
+  ridersArray = riders.map((item): any => GetUserLatLong(item[0], item[1]));
+  let groups: any = {}; // Initialize groups as an empty object
+  ridersArray.forEach(([userId, cellId]) => {
     const group = groups[cellId.token()] || [];
     group.push(userId);
     groups[cellId.token()] = group;
   });
 
-  const searchPointLongLat = [-73.98991584777832, 40.69528168934989];
+  const searchPointLongLat = [-1.204948, 36.776498];
   const searchPointS2 = new s2.CellId(
     new s2.LatLng(searchPointLongLat[1], searchPointLongLat[0])
-  ).parent(13);
+  ).parent(9);
 
   console.log(searchPointS2.token()); // '89c25a4c'
   console.log(groups); // { '89c2595c': [ 'user1', 'user2' ], '89c25a4c': [ 'user3' ] }
@@ -44,3 +39,13 @@ export const GeoHarsh = (req: Request, res: Response) => {
   console.log(closePoints); // [ 'user3' ]
   return res.status(200).json(closePoints);
 };
+
+export const RiderPostLocation = (req: Request, res: Response) => {};
+export const UserGetNearbyRides = (req: Request, res: Response) => {};
+export const UserRequestRide = (req: Request, res: Response) => {};
+export const RiderGetRequestedRides = (req: Request, res: Response) => {};// websockets
+export const RiderAcceptRide = (req: Request, res: Response) => {};//websockets
+export const UserNotifiedRide = (req: Request, res: Response) => {};//websockets
+export const UserRiderShareLocation = (req: Request, res: Response) => {};//websockets
+export const UserCancelRide = (req: Request, res: Response) => {};//Rest -> driver websockets
+export const RiderCancelRide = (req: Request, res: Response) => {};//rest -> . user via sockets
--- src/Ride/Routes.ts
+++ src/Ride/Routes.ts
@@ -1,8 +1,17 @@
 import express from 'express';
-import { GeoHarsh } from './Controller';
+import {
+  RiderCancelRide,
+  RiderPostLocation,
+  UserCancelRide,
+  UserGetNearbyRides,
+  UserRequestRide,
+} from './Controller';
 
 const router = express.Router();
-// router.post('', CustomerCareAgent);
-router.get('/location', GeoHarsh);
+router.post('/rider-post-locations/', RiderPostLocation);
+router.get('/user-get-nearby-riders/', UserGetNearbyRides);
+router.post('/user-request-ride/', UserRequestRide);
+router.post('/user-cancel-ride/', UserCancelRide);
+router.post('/rider-cancel-ride/', RiderCancelRide);
 
 export default router;
