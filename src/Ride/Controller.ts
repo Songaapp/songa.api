f0bdfa1521119ecc94d9240fe17eb5d8aab02cae
Dennis Ngeno <dennis.ngeno.andela@alation.com>
2023-07-05 16:18:51 +0300

    - work on bugs

--- src/Ride/Controller.ts
+++ src/Ride/Controller.ts
@@ -1,101 +1,46 @@
 import { Request, Response } from 'express';
 import s2 from '@radarlabs/s2';
 import app from '../app';
+import { Prisma, PrismaClient } from '@prisma/client';
 
-const GetUserLatLong = (userId: string, LongLat: [number, number]) => {
-  return [
-    userId,
-    new s2.CellId(new s2.LatLng(LongLat[1], LongLat[0])).parent(10),
-  ];
-};
+const prisma = new PrismaClient({});
 
-const riders: Array<Array<any>> = [
-  ['r1', [-1.201726, 36.785418]],
-  ['r2', [-1.207343, 36.786401]],
-  ['r3', [-1.204948, 36.776498]],
-  ['r4', [-1.094669, 36.631879]],
-  ['r5', [-1.206408, 36.791688]],
-];
-
-export const GeoHarsh = (req: Request, res: Response) => {
-  // let ridersArray = [];
-  let ridersArray = riders.map((item): any => GetUserLatLong(item[0], item[1]));
-  let groups: any = {}; // Initialize groups as an empty object
-  ridersArray.forEach(([userId, cellId]) => {
-    const group = groups[cellId.token()] || [];
-    group.push(userId);
-    groups[cellId.token()] = group;
-  });
-
-  const searchPointLongLat = [-1.204948, 36.776498];
-  const searchPointS2 = new s2.CellId(
-    new s2.LatLng(searchPointLongLat[1], searchPointLongLat[0])
-  ).parent(9);
-  // import { Request, Response } from 'express';
-  // import s2 from '@radarlabs/s2';
-
-  // export const GeoHarsh = (req: Request, res: Response) => {
-  //   const user1LongLat = [-73.95772933959961, 40.71623280185081];
-  //   const user2LongLat = [-73.95927429199219, 40.71629785715124];
-  //   const user3LongLat = [-73.99206161499023, 40.688708709249646];
-
-  //   interface Group {
-  //     userId: string;
-  //     cellId: any;
-  //   }
-
-  //   const user1S2: [string, any] = [
-  //     'user1',
-  //     new s2.CellId(new s2.LatLng(user1LongLat[1], user1LongLat[0])).parent(13),
-  //   ];
-  //   const user2S2: [string, any] = [
-  //     'user2',
-  //     new s2.CellId(new s2.LatLng(user2LongLat[1], user2LongLat[0])).parent(13),
-  //   ];
-  //   const user3S2: [string, any] = [
-  //     'user3',
-  //     new s2.CellId(new s2.LatLng(user3LongLat[1], user3LongLat[0])).parent(13),
-  //   ];
-
-  //   let groups: any; // Initialize groups as an empty object
-
-  //   [user1S2, user2S2, user3S2].forEach(([userId, cellId]) => {
-  //     const group = groups[cellId.token()] || [];
-  //     group.push(userId);
-  //     groups[cellId.token()] = group;
-  //   });
-
-  //   const searchPointLongLat = [-73.98991584777832, 40.69528168934989];
-  //   const searchPointS2 = new s2.CellId(
-  //     new s2.LatLng(searchPointLongLat[1], searchPointLongLat[0])
-  //   ).parent(13);
-  // >>>>>>> df06704810758d17f7240286b939afcb3eba41b6
-
-  //   console.log(searchPointS2.token()); // '89c25a4c'
-  //   console.log(groups); // { '89c2595c': [ 'user1', 'user2' ], '89c25a4c': [ 'user3' ] }
-
-  // <<<<<<< HEAD
-  const closePoints = groups[searchPointS2.token()];
-  console.log(closePoints); // [ 'user3' ]
-  return res.status(200).json(closePoints);
-};
-
-export const RiderPostLocation = (req: any, res: Response) => {
+export const RiderPostLocation = async (req: any, res: Response) => {
   if (typeof app.locals.groups == 'undefined') {
     app.locals.groups = {};
   }
-  const { latitude, longitude } = req.body;
-  const rider_cell_id = new s2.CellId(new s2.LatLng(latitude, longitude))
-    .parent(11)
-    .token();
-  const group = app.locals.groups[rider_cell_id] || new Set();
-  group.add(req.payload.id);
-  app.locals.groups[rider_cell_id] = group;
-  // console.log(app.locals.groups);
-  return res.status(200).json(app.locals.groups);
+  try {
+    const { latitude, longitude } = req.body;
+    await prisma.rider.update({
+      where: {
+        id: res.locals.payload.id,
+      },
+      data: {
+        ...req.body,
+      },
+    });
+    const rider_cell_id = new s2.CellId(new s2.LatLng(latitude, longitude))
+      .parent(11)
+      .token();
+    const group = app.locals.groups[rider_cell_id] || new Set();
+    // group.add(req.payload.id);
+    app.locals.groups[rider_cell_id] = group;
+    return res
+      .status(200)
+      .json({ body: req.body, message: 'Location posted successfully' });
+  } catch (err) {
+    if (err instanceof Prisma.PrismaClientKnownRequestError) {
+      // The .code property can be accessed in a type-safe manner
+      if (err.code === 'P2025') {
+        res.status(404).json({ message: 'Rider not found' });
+        return;
+      }
+    }
+    return res.status(500).send({ message: 'Unknown error' });
+  }
 };
 
-export const UserGetNearbyRides = (req: Request, res: Response) => {
+export const UserGetNearbyRides = async (req: Request, res: Response) => {
   if (typeof app.locals.groups == 'undefined') {
     app.locals.groups = {};
   }
@@ -105,13 +50,23 @@ export const UserGetNearbyRides = (req: Request, res: Response) => {
   ).parent(11);
   console.log(app.locals.groups);
   const close_rider_points = app.locals.groups[customer_point_s2.token()];
-  // console.log(closePoints); // [ 'user3' ]
   if (!close_rider_points) {
     return res.status(404).json({ message: 'No riders found' });
   }
+  const records = await prisma.rider.findMany({
+    select: {
+      latitude: true,
+      longitude: true,
+    },
+    where: {
+      id: {
+        in: Array.from(close_rider_points),
+      },
+    },
+  });
   return res.status(200).json({
     message: 'Riders found',
-    location: Array.from(close_rider_points),
+    locations: Array.from(records),
   });
 };
 export const UserRequestRide = (req: Request, res: Response) => {};
