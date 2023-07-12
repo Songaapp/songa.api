331f51c86c948f91adc805230346c28412d5801c
Dennis Ngeno <dennis.ngeno.andela@alation.com>
2023-07-12 12:52:10 +0300

    - removed the rename done by martin

--- src/Ride Matching/Routes.ts
+++
@@ -1,8 +0,0 @@
-// import express from 'express';
-// import { GeoHarsh } from './Controller';
-
-// const router = express.Router();
-// // router.post('', CustomerCareAgent);
-// router.get('/location', GeoHarsh);
-
-// export default router;
